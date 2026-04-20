6ab6e04bf679122a5c1b0dfd399baedbaf14ec06
MonoChronos <85042666+monoc0@users.noreply.github.com>
2026-04-20 10:36:33 +0800

    2d list

--- main.py
+++ main.py
@@ -15,12 +15,17 @@ title = r"""
 a demo for CS2
 """
 
-menu = ['Adobo', 'Pancit', 'Sinigang', 'Lechon', 'Tinola', 'Kaldereta', 'Kare Kare', 'Arroz Caldo', 'Lumpia', 'Bulalo', 'Bicol express', 'Tapsilog', 'Lomi', 'Bistek Beef'
+menu = [
+  ['Adobo', 'Pancit', 'Sinigang'],
+  ['Lechon', 'Tinola', 'Kaldereta'],
+  ['Kare Kare', 'Arroz Caldo', 'Lumpia'],
+  ['Bulalo', 'Bicol express', 'Tapsilog'],
+  ['Lomi', 'Bistek Beef', 'Chicken Inasal']
 ]
 dessert = ['Bibingka', 'Halo-Halo', 'Taho', 'Champarado(tm)', 'Leche flan', 'Mango float']
 
-time.sleep(5)
 print(title)
 time.sleep(5)
 user = input("Enter your username")
 print(f"Welcome {user} to our program! Here you will explore the vast Filipino cuisine, and discover famous recipes and facts of Philippine cuisine!")
+
