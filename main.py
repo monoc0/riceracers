1a2cffe262fdb21b9c2a70de6ad45c34a99d11f0
MonoChronos <contact.monoc@gmail.com>
2026-04-20 10:57:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,16 @@ main = ['Adobo', 'Sinigang', 'Lechon']
 side = ['Lomi', 'Sopas', 'Munggo']
 dessert = ['Halo-Halo', 'Taho', 'Leche flan']
 drinks = ['Gulaman', 'Buko Juice', 'Magic Water']
+menu = [main,side,dessert,drinks]
 
 print(title)
-time.sleep(5)
-user = input("Enter your username")
+time.sleep(2)
+user = input("Enter your username: ").strip()
 print(f"Welcome {user} to our program! Here you will explore the vast Filipino cuisine, and discover famous recipes and facts of Philippine cuisine!")
-
+time.sleep(2)
+for cat in menu:
+    print()
+    for item in cat:
+        print(f" * {item}",end="")
+    print()
+    print("-"*67, end='')
