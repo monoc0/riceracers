baaaa480c63cafe5698960c4365419dae838febf
skibidiskibiditoilet2525 <francioxian@gmail.com>
2026-05-03 14:26:16 +0800

    Update modules.py

--- modules.py
+++ modules.py
@@ -8,7 +8,7 @@ menu = [main,side,dessert,drinks]
 
 main_desc = {'Adobo':"Adobo, a national delicacy unique for it's savory taste and pride of the Filipinos. Adobo is flavorful with its combination soy sauce, vinegar, sugar, peppercorns, bay leaves and lots more! '', 'Sinigang': 'A sour soup usually using shrimp or pork added with tamarind, vegtables, and gabi to enhance its flavor!', 'Lechon': 'A whole roasted pig typically served in parties or gatherings, known for it crispy skin, fatty and delicious meat!
 \n Lechon is stuffed with a multitude of herbs and spices inside the body of the pig, when cutting it open you will be hit with an array of flavorous smells!'}
-side_desc = {'Lomi': 'a', 'Sopas': 'a', 'Munggo': 'a'}
+side_desc = {'Lomi': '', 'Sopas': 'Sopas is a creamy macaroni soup paired with hotdog and vegtables, and typically added with evaporated milk!', 'Munggo': 'a'}
 dessert_desc = {'Halo-Halo': 'a', 'Taho': 'a', 'Leche Flan': 'a'}
 drinks_desc = {'Gulaman': 'a', 'Buko Juice': 'a', 'Magic Water': 'a'}
 menu_desc = [main_desc,side_desc,dessert_desc,drinks_desc]
